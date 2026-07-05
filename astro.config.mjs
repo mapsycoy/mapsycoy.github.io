b6271bf849108136878900980c82708f43f6b245
Junyeop Kim <mapsycoy@gmail.com>
2026-07-05 13:50:21 +0900

    Add Markdown footnote support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from "astro/config";
 import { unified } from "@astrojs/markdown-remark";
+import remarkGfm from "remark-gfm";
 import rehypeLinkPreviews from "./src/utils/rehypeLinkPreviews.mjs";
 import rehypeSmartTypography from "./src/utils/smartTypography.mjs";
 
 export default defineConfig({
   site: "https://mapsycoy.github.io",
   markdown: unified({
+    remarkPlugins: [remarkGfm],
     rehypePlugins: [rehypeSmartTypography, rehypeLinkPreviews],
   }),
 });
